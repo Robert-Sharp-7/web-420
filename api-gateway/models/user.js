d8052a42e5f2770cc750588d03f0b651ea78974e
Robert Sharp <robert.sharp.dev@gmail.com>
2018-05-25 02:07:43 -0500

    Week 4

---
+++ api-gateway/models/user.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
+
+//user.save to add new database user
+module.exports.add = (user, callback) => {
+  user.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+  var query = {_id: id};
+  User.findById(query, callback);
+};
